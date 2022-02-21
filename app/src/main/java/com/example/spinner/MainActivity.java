cd4a37e4f45a224bbccca46064f4b8bcfa6c19eb
ajayvamsee24 <ajay.vamsee@techolution.com>
2022-02-21 12:50:46 +0530

    spinner is implemented with image

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,16 +3,60 @@ package com.example.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     float brightness=10f;
 
+    Button btnSpin;
+    ImageView ivWheel;
+    CountDownTimer timer;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSpin=findViewById(R.id.btnSpin);
+        ivWheel=findViewById(R.id.ivWheel);
+
+        Random random=new Random();
+
+        btnSpin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnSpin.setEnabled(false);
+
+                // Random values blw 10 to 30
+                int spin =random.nextInt(20)+10;
+
+                // since the wheel has 10 divisions, the
+                // rotation should be a multiple of
+                // 360/10 = 36 degrees
+                spin=spin*36;
+
+                timer=new CountDownTimer(spin*5,1) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        float rotation=ivWheel.getRotation()+2.5f;
+                        ivWheel.setRotation(rotation);
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        btnSpin.setEnabled(true);
+                    }
+                }.start();
+            }
+        });
 
         //screenBright(brightness);
     }
